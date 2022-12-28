938ca76ebb78cfa08d7cc81165b17f06ab49a4e2
Hans Ellis <bullhorn@gmail.com>
2022-12-27 20:13:47 -0800

    add hairsaloncontext file

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -1,2 +1,13 @@
 using Microsofgt.EntityFrameworkCore;
 
+namespace HairSalon.Models
+{
+  public class HairSalonContext : dbContext
+  {
+    public DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Client> Clients { get; set; }
+
+    public ToDoListContext(DbContextOptions options) : base(options) { }
+  }
+
+}
